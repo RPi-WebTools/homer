28488d8ca688646beea07980efcd282840b4e93c
Viperinius <viperinius@gmx.de>
2021-04-08 22:54:20 +0200

    modified lynxl logo slightly and added favicon

--- vue.config.js
+++ vue.config.js
@@ -18,8 +18,8 @@ module.exports = {
     themeColor: manifestOptions.theme_color,
     manifestOptions,
     iconPaths: {
-      favicon32: "assets/icons/favicon-32x32.png",
-      favicon16: "assets/icons/favicon-16x16.png",
+      favicon32: "assets/icons/favicon32.png",
+      favicon16: "assets/icons/favicon16.png",
       appleTouchIcon: "assets/icons/icon-maskable.png",
       maskIcon: "assets/icons/safari-pinned-tab.svg",
       msTileImage: "assets/icons/icon-any.png",
